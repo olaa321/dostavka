4e298f1c32557456121eaf295f1dee618c3a0618
olaa321 <olaa15444@gmail.com>
2024-05-26 18:31:29 +0300

    f

--- pon.js
+++ pon.js
@@ -1,6 +1,5 @@
-
-const modal2 = document.querySelector('.backdrop');
-const modalBtnOpen2 = document.querySelector('.modal-bth-open');
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelector('.modal-bth-open');
 
 const modalBtnClose = document.querySelector('.modal-bth-close');
 
@@ -8,8 +7,14 @@ const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
-// chiken_burger.addEventListener("click", function () {
-//     let toggleModal = () => modal.classList.toggle('is-hidden');
-   
 
-// })
+
+const modal2 = document.querySelector('.backdrop2');
+const modalBtnOpen2 = document.querySelector('.modal-bth-open2');
+
+const modalBtnClose2 = document.querySelector('.modal-bth-close2');
+
+const toggleModal2 = () => modal2.classList.toggle('is-hidden2');
+
+modalBtnOpen2.addEventListener('click', toggleModal2);
+modalBtnClose2.addEventListener('click', toggleModal2);
